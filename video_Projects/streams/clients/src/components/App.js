9041c07a4f08cf1e8890978982ec1a55ad2ee558
elgin <elginm08@gmail.com>
2021-09-16 14:31:48 +0200

    ch_20 complete

--- video_Projects/streams/clients/src/components/App.js
+++ video_Projects/streams/clients/src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
-import StreamEdit from './StreamEdit';
+import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
-import StreamList from './StreamList';
-import StreamShow from './StreamShow';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 import Header from './Header';
 
 const App = () => {
